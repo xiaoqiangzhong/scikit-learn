0ca53b238b7029f0b7687df7b391d4a52bf0a153
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-05-26 22:30:54 +0200

    faster!

--- sklearn/cluster/_k_means_elkan.pyx
+++ sklearn/cluster/_k_means_elkan.pyx
@@ -2,6 +2,8 @@ import numpy as np
 cimport numpy as np
 cimport cython
 
+from libc.math cimport sqrt
+
 from ..metrics import euclidean_distances
 from .k_means_ import _tolerance
 from ._k_means import _centers_dense
@@ -10,39 +12,47 @@ from ._k_means import _centers_dense
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-cdef d(double[:] a, double[:] b):
+cdef double d(double[:] a, double[:] b) nogil:
     cdef double result = 0
     cdef int i
-    for i in range(len(a)):
+    for i in range(a.shape[0]):
         result += (a[i] - b[i]) ** 2
-    return np.sqrt(result)
+    return sqrt(result)
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
 cdef assign_labels(double[:, :] X, double[:, :] centers, double[:, :]
-                   center_distances):
+                   center_distances, int[:] labels, double[:] distances):
+    print("start assign labels")
     # assigns closest center to X
     # uses triangle inequality
-    new_centers, distances = [], []
+    cdef int n_samples = X.shape[0]
+    cdef int n_centers = centers.shape[0]
+    cdef double[:] x, c
     cdef double d_c, dist
-    cdef int c_x
-    for x in X:
+    cdef int c_x, j, sample
+    for sample in range(n_samples):
         # assign first cluster center
         c_x = 0
+        x = X[sample]
         d_c = d(x, centers[0])
-        for j, c in enumerate(centers):
+        for j in range(n_centers):
+            c = centers[j]
             if d_c > center_distances[c_x, j]:
                 dist = d(x, c)
                 if dist < d_c:
                     d_c = dist
                     c_x = j
-        new_centers.append(c_x)
-        distances.append(d_c)
-    return np.array(new_centers, dtype=np.int32), np.array(distances)
+        labels[sample] = c_x
+        distances[sample] = d_c
+    print("end assign labels")
 
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
 def k_means_elkan(X, n_clusters, init, float tol=1e-4, int max_iter=30, verbose=False):
     #initialize
     tol = _tolerance(X, tol)
@@ -53,48 +63,56 @@ def k_means_elkan(X, n_clusters, init, float tol=1e-4, int max_iter=30, verbose=
     cdef float upper_bound, distance
     cdef double[:, :] center_distances = euclidean_distances(centers) / 2.
     cdef double[:, :] lower_bounds = np.zeros((n_samples, n_centers))
-    labels, upper_bounds = assign_labels(
-        X, centers, center_distances)
+    cdef double[:] x, distance_next_center
+    labels_ = np.empty(n_samples, dtype=np.int32)
+    cdef int[:] labels = labels_
+    upper_bounds_ = np.empty(n_samples, dtype=np.float)
+    cdef double[:] upper_bounds = upper_bounds_
+    assign_labels(X, centers, center_distances, labels, upper_bounds)
     # make bounds tight for current labelss
     for point_index in range(n_samples):
         lower_bounds[point_index, labels[point_index]] = upper_bounds[point_index]
-    bounds_tight = np.ones(n_samples, dtype=np.bool)
+    cdef np.uint8_t[:] bounds_tight = np.ones(n_samples, dtype=np.uint8)
+    cdef np.uint8_t[:] points_to_update = np.zeros(n_samples, dtype=np.uint8)
     for iteration in range(max_iter):
+        print("start iteration")
         distance_next_center = np.sort(center_distances, axis=0)[1]
-        points_to_update = distance_next_center[labels] < upper_bounds
-        for point_index in np.where(points_to_update)[0]:
+        for point_index in range(n_samples):
+            if distance_next_center[labels[point_index]] >= upper_bounds[point_index]:
+                continue
             upper_bound = upper_bounds[point_index]
             label = labels[point_index]
+            x = X[point_index]
             # check other update conditions
-            for center_index, center in enumerate(centers):
+            for center_index in range(n_centers):
                 if (center_index != label
                         and (upper_bound > lower_bounds[point_index, center_index])
                         and (upper_bound > center_distances[center_index, label])):
                     # update distance to center
                     if not bounds_tight[point_index]:
-                        upper_bound = d(X[point_index], centers[label])
+                        upper_bound = d(x, centers[label])
                         lower_bounds[point_index, label] = upper_bound
-                        bounds_tight[point_index] = True
+                        bounds_tight[point_index] = 1
                     # check for relabels
                     if (upper_bound > lower_bounds[point_index, center_index]
                             or (upper_bound > center_distances[label, center_index])):
-                        distance = d(X[point_index], center)
+                        distance = d(x, centers[center_index])
                         lower_bounds[point_index, center_index] = distance
                         if distance < upper_bound:
                             label = center_index
                             upper_bound = distance
             labels[point_index] = label
             upper_bounds[point_index] = upper_bound
-
+        print("end inner loop")
         # compute new centers
-        new_centers = _centers_dense(X, labels, n_centers, upper_bounds)
-        bounds_tight = np.zeros(n_samples, dtype=np.bool)
+        new_centers = _centers_dense(X, labels_, n_centers, upper_bounds_)
+        bounds_tight = np.zeros(n_samples, dtype=np.uint8)
 
         # compute distance each center moved
         center_shift = np.sqrt(np.sum((centers - new_centers) ** 2, axis=1))
         # update bounds accordingly
         lower_bounds = np.maximum(lower_bounds - center_shift, 0)
-        upper_bounds = upper_bounds + center_shift[labels]
+        upper_bounds = upper_bounds + center_shift[labels_]
         # reassign centers
         centers = new_centers
         # update between-center distances
