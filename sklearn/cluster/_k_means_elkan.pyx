b9bed19efba9f23617a6fe474bea239813de875a
Andreas Mueller <amueller@ais.uni-bonn.de>
2013-05-27 16:11:03 +0200

    pointer kung-fu makes it faster :-/

--- sklearn/cluster/_k_means_elkan.pyx
+++ sklearn/cluster/_k_means_elkan.pyx
@@ -17,35 +17,40 @@ from .k_means_ import _tolerance
 from ._k_means import _centers_dense
 
 
-cdef double d(double[:] a, double[:] b) nogil:
+cdef double *getfloatpointer(np.ndarray[np.float64_t, ndim=2, mode='c'] data):
+    return <double *>(data.data) 
+
+
+cdef double d(double* a, double* b, int n_features) nogil:
     cdef double result, tmp
     result = 0
     cdef int i
-    for i in range(a.shape[0]):
-        tmp = (a[i] - b[i])
+    for i in range(n_features):
+        tmp = (a[0] - b[0])
+        a += 1
+        b += 1
         result += tmp * tmp
     return sqrt(result)
 
 
-cdef assign_labels(double[:, :] X, double[:, :] centers, double[:, :]
-                   center_distances, int[:] labels, double[:] distances):
+cdef assign_labels(double* X, double* centers, double[:, :]
+                   center_distances, int[:] labels, double[:] distances, int n_samples, int n_features, int n_centers):
     print("start assign labels")
     # assigns closest center to X
     # uses triangle inequality
-    cdef int n_samples = X.shape[0]
-    cdef int n_centers = centers.shape[0]
-    cdef double[:] x, c
+    cdef double* x
+    cdef double* c
     cdef double d_c, dist
     cdef int c_x, j, sample
     for sample in range(n_samples):
         # assign first cluster center
         c_x = 0
-        x = X[sample]
-        d_c = d(x, centers[0])
+        x = X + sample * n_features
+        d_c = d(x, centers, n_features)
         for j in range(n_centers):
             if d_c > center_distances[c_x, j]:
-                c = centers[j]
-                dist = d(x, c)
+                c = centers + j * n_features
+                dist = d(x, c, n_features)
                 if dist < d_c:
                     d_c = dist
                     c_x = j
@@ -60,7 +65,11 @@ def k_means_elkan(X_, n_clusters, init, float tol=1e-4, int max_iter=30, verbose
     centers_ = init
     cdef double[:, :] centers = centers_
     cdef double[:, :] X = X_
+    cdef double* centers_p = getfloatpointer(centers_)
+    cdef double* X_p = getfloatpointer(X_)
+    cdef double* x_p
     cdef int n_samples = X.shape[0]
+    cdef int n_features = X.shape[1]
     cdef int n_centers = centers.shape[0]
     cdef int point_index, center_index, label
     cdef float upper_bound, distance
@@ -71,7 +80,7 @@ def k_means_elkan(X_, n_clusters, init, float tol=1e-4, int max_iter=30, verbose
     cdef int[:] labels = labels_
     upper_bounds_ = np.empty(n_samples, dtype=np.float)
     cdef double[:] upper_bounds = upper_bounds_
-    assign_labels(X, centers, center_distances, labels, upper_bounds)
+    assign_labels(X_p, centers_p, center_distances, labels, upper_bounds, n_samples, n_features, n_centers)
     # make bounds tight for current labelss
     for point_index in range(n_samples):
         lower_bounds[point_index, labels[point_index]] = upper_bounds[point_index]
@@ -88,7 +97,7 @@ def k_means_elkan(X_, n_clusters, init, float tol=1e-4, int max_iter=30, verbose
             label = labels[point_index]
             if distance_next_center[label] >= upper_bound:
                 continue
-            x = X[point_index]
+            x_p = X_p + point_index * n_features
             # check other update conditions
             for center_index in range(n_centers):
                 if (center_index != label
@@ -96,13 +105,13 @@ def k_means_elkan(X_, n_clusters, init, float tol=1e-4, int max_iter=30, verbose
                         and (upper_bound > center_distances[center_index, label])):
                     # update distance to center
                     if not bounds_tight[point_index]:
-                        upper_bound = d(x, centers[label])
+                        upper_bound = d(x_p, centers_p + label * n_features, n_features)
                         lower_bounds[point_index, label] = upper_bound
                         bounds_tight[point_index] = 1
                     # check for relabels
                     if (upper_bound > lower_bounds[point_index, center_index]
                             or (upper_bound > center_distances[label, center_index])):
-                        distance = d(x, centers[center_index])
+                        distance = d(x_p, centers_p + center_index * n_features, n_features)
                         lower_bounds[point_index, center_index] = distance
                         if distance < upper_bound:
                             label = center_index
@@ -122,6 +131,7 @@ def k_means_elkan(X_, n_clusters, init, float tol=1e-4, int max_iter=30, verbose
         # reassign centers
         centers = new_centers
         centers_ = new_centers
+        centers_p = getfloatpointer(new_centers)
         # update between-center distances
         center_distances = euclidean_distances(centers_) / 2.
         if verbose:
